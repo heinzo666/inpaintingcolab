87eab99b69099982440826bfea8563e4319118c7
heinzo666 <166720679+heinzo666@users.noreply.github.com>
2024-07-22 20:57:50 +0700

    Update training.py

--- training.py
+++ training.py
@@ -2,14 +2,14 @@ import os
 
 from git import Repo
 import datetime as dt
-tgl = dt.datetime.now().strftime("%y%m%d_%H%M%S")
-os.chdir('/content/text')
+tgl = dt.datetime.now().strftime("%y%m%d_%H%M")
+os.chdir('/content/inpaintingcolab')
 
-full_local_path = "/content/text"
+full_local_path = "/content/inpaintingcolab"
 
 repo = Repo(full_local_path)
 repo.git.add("-A")
-repo.index.commit("user_fakes")
+repo.index.commit(tgl)
 
 origin = repo.remote(name="origin")
 origin.push()
